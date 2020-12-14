5760342f57278c5896dbf5b2ed5dfc97d3e47f97
dhchun1203 <dhchun1203@gmail.com>
2020-12-14 16:23:15 +0900

    JS: Navbar menu toggle + Project UI tweak

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document.addEventListener("scroll", () => {
 		navbar.classList.remove("navbar--dark");
 	}
 });
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
@@ -17,19 +18,29 @@ navbarMenu.addEventListener("click", (event) => {
 	if (link == null) {
 		return;
 	}
+	navbarMenu.classList.remove("open");
 	scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+	navbarMenu.classList.toggle("open");
+});
+
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", (event) => {
 	scrollIntoView("#contact");
 });
+
 // Make home slowly fade to transparent with window scroll down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
 	home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
@@ -39,6 +50,7 @@ document.addEventListener("scroll", () => {
 		arrowUp.classList.remove("visible");
 	}
 });
+
 // Handle click on the "arrow up" button
 arrowUp.addEventListener("click", () => {
 	scrollIntoView("#home");
